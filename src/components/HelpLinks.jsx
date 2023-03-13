cdd1e6f4f0934013e9730cdbb0660918771415a9
Matt <40559749+mattskeoch@users.noreply.github.com>
2023-03-13 18:07:06 +0800

    &apos;

--- src/components/HelpLinks.jsx
+++ src/components/HelpLinks.jsx
@@ -25,7 +25,7 @@ export const HelpLinks = () => {
                   <p className="font-bold">
                     You can set up a signature in the Outlook desktop
                     application by copying and pasting your signature text into
-                    the Signature section under the Mail tab in Outlook's
+                    the Signature section under the Mail tab in Outlook&apos;s
                     Settings.
                   </p>
                   <ol className="mt-5 ml-8 list-decimal space-y-4">
@@ -74,7 +74,7 @@ export const HelpLinks = () => {
                   <p className="font-bold">
                     You can set up a signature in the Outlook.com application by
                     copying and pasting your signature into the Email Signature
-                    section Outlook.com's settings.
+                    section Outlook.com&apos;s settings.
                   </p>
                   <ol className="mt-5 ml-8 list-decimal space-y-4">
                     <li>
@@ -84,15 +84,18 @@ export const HelpLinks = () => {
                     <li>
                       <p>
                         Head to Outlook.com and click on the gear icon in the
-                        top right-hand corner and select 'View all Outlook
-                        settings'.
+                        top right-hand corner and select &apos;View all Outlook
+                        settings&apos;.
                       </p>
                       <li>
-                        <p>Scroll down to the 'Email signature' section.</p>
+                        <p>
+                          Scroll down to the &apos;Email signature&apos;
+                          section.
+                        </p>
                       </li>
                     </li>
                     <li>
-                      <p>Then, go to 'Compose and reply'.</p>
+                      <p>Then, go to &apos;Compose and reply&apos;.</p>
                     </li>
                     <li>
                       <p>Here, you can Paste in the signature you created.</p>
@@ -118,7 +121,10 @@ export const HelpLinks = () => {
                 </AlertDialogTitle>
                 <AlertDialogDescription>
                   You can email
-                  <a href="mailto:matt@tribe41.com.au"> matt@tribe41.com.au</a>{' '}
+                  <a href="mailto:matt@tribe41.com.au">
+                    {' '}
+                    matt@tribe41.com.au
+                  </a>{' '}
                   for further assistance.
                 </AlertDialogDescription>
               </AlertDialogHeader>
